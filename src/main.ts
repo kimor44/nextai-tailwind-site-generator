1489069e10b2be346cb2a065dfe42123560a3d56
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2024-02-08 11:25:21 +0100

    Mise à jour de la génération de code HTML avec OpenAI

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import chatCompletion from "./lib/openAi/sendPrompt";
+import { openai } from "./lib/openAi/openAiKey";
 
 const iframe = document.querySelector("#iframe-generated") as HTMLIFrameElement;
 
@@ -7,10 +7,56 @@ input.addEventListener("submit", async (event) => {
   event.preventDefault();
   const formData = new FormData(event.currentTarget as HTMLFormElement);
   const prompt = formData.get("generator__textarea") as string;
-  const response = await chatCompletion(prompt);
-  const content = response.choices[0].message.content;
-  if (!content) return;
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `Tu crées des sites web.
+      Tu es un développeur web.
+      Ta tâche est de créer un site web avec tailwindcss.
+      Tu ne renvoies que du code HTML.
+      Tu ne mets rien avant et après le code HTML.
+      Tu ne mets pas de commentaire dans le code HTML.
+      Tu ne mets pas de Markdown dans le code HTML.
+      Tu ne mets pas de backticks dans le code HTML. (avant et après le code HTML)
+      Si le prompt ne te demande pas un élément d'un site web, renvoie une erreure dans une balise "div" avec un fond rouge clair et le texte en rouge foncé et gras.
+      `,
+      },
+      { role: "user", content: prompt },
+    ],
+    model: "gpt-3.5-turbo",
+    stream: true,
+  });
 
+  let code = "";
+  const onNewChunk = createUpdateIframe();
+
+  for await (const chunk of chatCompletion) {
+    const newChild = chunk.choices[0]?.delta?.content || "";
+    code += newChild;
+    onNewChunk(code);
+  }
+});
+
+const createUpdateIframe = () => {
+  let date = Date.now();
+  let timeout: any = null;
+
+  return (code: string) => {
+    if (Date.now() - date > 1000) {
+      updateIframe(code);
+      date = Date.now();
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        updateIframe(code);
+        date = Date.now();
+      }, 1000);
+    }
+  };
+};
+
+const updateIframe = (code: string) => {
   iframe.srcdoc = `
   <!DOCTYPE html>
     <html lang="en">
@@ -19,8 +65,8 @@ input.addEventListener("submit", async (event) => {
         <title>Generated code with AI</title>
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <body>
-        ${content}
+      <body id="body-generated" class="body-generated">
+        ${code}
       </body>
     </html>`;
-});
+};
