ad2616900cff5e576aceae1d61b3469053c471e7
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2024-02-22 16:35:31 +0100

    Add proper h screen values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "screen-1/10": "10vh",
+        "screen-2/10": "20vh",
+        "screen-3/10": "30vh",
+        "screen-4/10": "40vh",
+        "screen-5/10": "50vh",
+        "screen-6/10": "60vh",
+        "screen-7/10": "70vh",
+        "screen-8/10": "80vh",
+        "screen-9/10": "90vh",
+        "screen-1/6": "16.666667vh",
+        "screen-2/6": "33.333333vh",
+        "screen-3/6": "50vh",
+        "screen-4/6": "66.666667vh",
+        "screen-5/6": "83.333333vh",
+        "screen-1/3": "33.333333vh",
+        "screen-2/3": "66.666667vh",
+        "screen-1/4": "25vh",
+        "screen-1/2": "50vh",
+        "screen-3/4": "75vh",
+      },
+    },
   },
   plugins: [],
 };
